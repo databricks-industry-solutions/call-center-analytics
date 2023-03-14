21f615afc0cb767ec56ff68b57f017a46926bff7
dbbnicole <dbbnicole>
2023-03-14 04:27:43 +0000

    init commit with Paul's notebooks, config extracted, with integration test automation

--- util/notebook-config.py
+++ util/notebook-config.py
@@ -1,33 +1,30 @@
 # Databricks notebook source
-# DBTITLE 1,Kafka config - see the RUNME notebook for instructions on setting up secrets
-kafka_bootstrap_servers = dbutils.secrets.get("solution-accelerator-cicd", "iot-anomaly-kafka-bootstrap-server")
-security_protocol = "SASL_SSL"
-sasl_mechanism = "PLAIN"
-sasl_username = dbutils.secrets.get("solution-accelerator-cicd", "iot-anomaly-sasl-username")
-sasl_password = dbutils.secrets.get("solution-accelerator-cicd", "iot-anomaly-sasl-password")
-topic = "iot_msg_topic"
-sasl_config = f'org.apache.kafka.common.security.plain.PlainLoginModule required username="{sasl_username}" password="{sasl_password}";'
+# MAGIC %md ## Set config
+
+# COMMAND ----------
+
+config = {}
 
 # COMMAND ----------
 
 # DBTITLE 1,Streaming checkpoint location
-checkpoint_path = "/dbfs/tmp/iot-anomaly-detection/checkpoints"
+config["checkpoint_path"] = "/tmp/delta/tutorials/librispeech"
 
 # COMMAND ----------
 
 # DBTITLE 1,Database settings
-database = "rvp_iot_sa"
-
-spark.sql(f"create database if not exists {database}")
+config["database"] = "libri"
+spark.sql(f"""create database if not exists {config["database"]}""")
+spark.sql(f"""use {config["database"]}""")
 
 # COMMAND ----------
 
 # DBTITLE 1,mlflow settings
 import mlflow
-model_name = "iot_anomaly_detection_xgboost"
+config["model_name"] = "libri"
 username = dbutils.notebook.entry_point.getDbutils().notebook().getContext().userName().get()
-mlflow.set_experiment('/Users/{}/iot_anomaly_detection'.format(username))
+mlflow.set_experiment('/Users/{}/libri'.format(username))
 
 # COMMAND ----------
 
-
+print(f"Set config: ", config)
